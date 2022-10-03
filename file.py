afb83ae4b8031b7c2073160f4c9dc7788d0bc7b2
gdavisiv <gdavisiv@gmail.com>
2022-10-03 16:35:29 +0000

    Added new comments to code, Added test file for reading image MetaData

--- file.py
+++ file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 #Importing Necessary Libraries
 import boto3
+
+#Iterates over all objects in a given s3 bucket
+#provides the following info:
+#
+#Key: filename
+#LastModified
+#ETag
+#size
+#StorageClass
 def iterate_bucket_items(bucket):
     client = boto3.client('s3')
     paginator = client.get_paginator('list_objects_v2')
@@ -11,13 +20,13 @@ def iterate_bucket_items(bucket):
                 yield item
 for i in iterate_bucket_items(bucket='aws-test-img-bucket'):
     print(i)
-    print(bucket)
-         
 
+#Iterates over all objects in s3 bucket and provides:
+#Key: names stored in the bucket
 s3client = boto3.client('s3')
 bucket = 'aws-test-img-bucket'
 startAfter = '2020'
-s3objects= s3client.list_objects_v2(Bucket=bucket, StartAfter=startAfter )
+s3objects= s3client.list_objects_v2(Bucket=bucket, StartAfter=startAfter)
 
 for object in s3objects['Contents']:
     print(object['Key']) 
